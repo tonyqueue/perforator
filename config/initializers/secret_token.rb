2d4e143ddf68aa9adb99eaa0eaed0fff91922e2f
Tony Quartarolo <tonyq@sonic.net>
2013-10-08 15:14:35 -0700

    Initial commit. Added capybara and rspec and suppressed test unit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Perforator::Application.config.secret_key_base = 'a8ca4762211e0939bacf8ab7b6157b5b73f4d5aa63d79fb7e09c659b75014989bc7b7cfc902614840b3b3d596c7ed5c9d67fa9ad38720c14945245d2318625bd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Perforator::Application.config.session_store :cookie_store, key: '_perforator_session'
