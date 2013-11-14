c929b1050eee01ac73cf45b7785149de44e50059
Tony Quartarolo <tonyq@sonic.net>
2013-11-14 15:18:44 -0800

    Customize heading and add submit button

--- spec/features/url_form_spec.rb
+++ spec/features/url_form_spec.rb
@@ -5,4 +5,15 @@ describe 'URL form', :type => :feature do
     visit '/'
     page.should have_field 'URL'
   end
+  it 'accepts URL' do
+    visit '/'
+    fill_in 'URL', :with => 'http://google.com'
+    click_button 'Measure!'
+  end
+  it 'has a heading "Perforator"' do
+    visit '/'
+    within 'h1' do
+      page.should have_text 'Perforator'
+    end
+  end
 end
